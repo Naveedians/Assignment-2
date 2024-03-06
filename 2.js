4d14746685d65489684d0933d8c69730c115ac1e
Naveed Ahmed <160510766+Naveedians@users.noreply.github.com>
2024-03-06 09:59:20 +0500

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,32 @@
+"use strict";
+//Data types
+//String
+let fullName = "Naveed";
+//number
+let studentNumber = 17;
+//boolean
+let isPresent = true;
+//null
+let nulVaue = null;
+//undefined
+let undValue = undefined;
+//symbol
+let s1 = Symbol();
+console.log(s1);
+//bigint
+let x = 1235363883;
+let y = 26528791309309;
+console.log(x + y);
+//array
+let playersName = ["Nabveed", "Afridi", "Dhoni"];
+console.log(playersName[2]);
+//variable declare typecases
+//camelCase
+let fruitName = "Orange";
+console.log(fruitName);
+//snake_case
+let my_name = "Naveed";
+console.log(my_name);
+//PascalCase
+let MyCountryName = "Pakistan";
+console.log(MyCountryName);
---
+++ 2.ts
@@ -0,0 +1,44 @@
+
+
+//Data types
+//String
+let fullName: string = "Naveed";
+
+//number
+let studentNumber:number=17;
+
+//boolean
+let isPresent: boolean = true;
+
+//null
+let nulVaue: null= null;
+
+//undefined
+let undValue: undefined= undefined;
+
+//symbol
+let s1:symbol=Symbol();
+console.log(s1);
+
+//bigint
+let x   =1235363883;
+let y  =26528791309309;
+console.log(x+y);
+
+//array
+let playersName:string[]=["Nabveed","Afridi","Dhoni"]
+console.log(playersName[2]);
+
+//variable declare typecases
+//camelCase
+let fruitName:string="Orange";
+console.log(fruitName);
+
+//snake_case
+let my_name :string ="Naveed"
+console.log(my_name);
+
+//PascalCase
+let MyCountryName:string="Pakistan"
+console.log(MyCountryName);
+
